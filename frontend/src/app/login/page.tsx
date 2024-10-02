bb5011fe94a07ff425952579b067555954fdb1b0
Daniel Chen <113078548+dchen024@users.noreply.github.com>
2024-10-02 14:43:25 -0400

    feat: add google oauth

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,20 +3,27 @@
 import { supabase } from '../../lib/supabaseClient';
 
 export default function Login() {
-  const handleOAuthLogin = async (provider: string) => {
+  const handleOAuthLogin = async (provider: 'github' | 'google') => {
     const { error } = await supabase.auth.signInWithOAuth({
-      provider: 'github',
+      provider,
     });
-    if (error) console.error('Error logging in with GitHub:', error.message);
+    if (error)
+      console.error(`Error logging in with ${provider}:`, error.message);
   };
 
   return (
-    <div className='flex items-center justify-center h-screen'>
+    <div className='flex flex-col items-center justify-center h-screen space-y-4'>
       <button
         onClick={() => handleOAuthLogin('github')}
         className='bg-gray-800 text-white px-4 py-2 rounded'
       >
         Sign in with GitHub
+      </button>
+      <button
+        onClick={() => handleOAuthLogin('google')}
+        className='bg-blue-500 text-white px-4 py-2 rounded'
+      >
+        Sign in with Google
       </button>
     </div>
   );
--- notes.md
+++ notes.md
@@ -4,3 +4,13 @@
 
 - change Homepage URL when deploying
 - add Logo to GitHub App Page
+
+## Google
+
+- change Homepage URL when deploying
+- add Logo to Google App
+- change publishing status to allow external google accounts
+
+# Login
+
+- Test with bad passwords
